58be56678bd7d775c3d65d941d030d788aa0bf6a
chisam0217 <60233725+chisam0217@users.noreply.github.com>
2022-10-22 23:43:32 -0700

    Add files via upload

---
+++ core/models/model_factory.py
@@ -0,0 +1,58 @@
+import os
+import torch
+from torch.optim import Adam
+from core.models import predrnn, predrnn_memory_decoupling
+from pthflops import count_ops
+
+class Model(object):
+    def __init__(self, configs):
+        self.configs = configs
+        self.num_hidden = [int(x) for x in configs.num_hidden.split(',')]
+        self.num_layers = len(self.num_hidden)
+        networks_map = {
+            'predrnn': predrnn.RNN,
+            'predrnn_memory_decoupling': predrnn_memory_decoupling.RNN,
+        }
+
+        if configs.model_name in networks_map:
+            Network = networks_map[configs.model_name]
+            self.network = Network(self.num_layers, self.num_hidden, configs).to(configs.device)
+        else:
+            raise ValueError('Name of network unknown %s' % configs.model_name)
+
+        self.optimizer = Adam(self.network.parameters(), lr=configs.lr)
+
+    def save(self, itr):
+        stats = {}
+        stats['net_param'] = self.network.state_dict()
+        checkpoint_path = os.path.join(self.configs.save_dir, 'model.ckpt'+'-'+str(itr))
+        torch.save(stats, checkpoint_path)
+        print("save model to %s" % checkpoint_path)
+
+    def load(self, checkpoint_path):
+        print('load model:', checkpoint_path)
+        stats = torch.load(checkpoint_path)
+        self.network.load_state_dict(stats['net_param'])
+
+    def train(self, frames, mask):
+        frames_tensor = torch.FloatTensor(frames).to(self.configs.device)
+        mask_tensor = torch.FloatTensor(mask).to(self.configs.device)
+        self.optimizer.zero_grad()
+        next_frames, loss = self.network(frames_tensor, mask_tensor)
+        loss.backward()
+        self.optimizer.step()
+        return loss.detach().cpu().numpy()
+
+    def test(self, frames, mask):
+        frames_tensor = torch.FloatTensor(frames).to(self.configs.device)
+        mask_tensor = torch.FloatTensor(mask).to(self.configs.device)
+        next_frames, _ = self.network(frames_tensor, mask_tensor)
+        return next_frames.detach().cpu().numpy()
+
+    # def test(self, frames, mask):
+    #     frames_tensor = torch.FloatTensor(frames).to(self.configs.device)
+    #     mask_tensor = torch.FloatTensor(mask).to(self.configs.device)
+    #     # print ('frames_tensor', frames_tensor.size())
+    #     flops = count_ops(self.network, (frames_tensor, mask_tensor))
+    #     # print ('next_frames', next_frames.size())
+    #     return next_frames.detach().cpu().numpy(), flops
